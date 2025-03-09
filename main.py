70deefebbf12c35c701a81a91b97573edb5f3e91
Jazzy <jparkdev63@gmail.com>
2025-03-09 13:14:29 -0400

    Update

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
+import uvicorn
+import os
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/quote")
 async def say_quote():
     return {"message": "Success is falling 9 times and getting up 10 times."}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
